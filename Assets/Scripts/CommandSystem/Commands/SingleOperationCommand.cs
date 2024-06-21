bb12597295f7d6f448a2a6ff9b64c8c35255d671
Hunter Dyar <h.dyar@chatham.edu>
2024-06-21 14:22:49 -0400

    command system

---
+++ Assets/Scripts/CommandSystem/Commands/SingleOperationCommand.cs
@@ -0,0 +1,27 @@
+﻿using Clayze;
+using Clayze.Marching.Operations;
+
+namespace CommandSystem.Commands
+{
+	public class SingleOperationCommand : ICommand
+	{
+		private IOperation _op;
+		private OperationCollection _collection;
+
+		public SingleOperationCommand(OperationCollection collection, IOperation op)
+		{
+			_op = op;
+			_collection = collection;
+		}
+		
+		public void Execute()
+		{
+			_collection.Add(_op);
+		}
+
+		public void Undo()
+		{
+			_collection.Remove(_op);
+		}
+	}
+}
